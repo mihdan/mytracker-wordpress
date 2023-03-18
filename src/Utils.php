23e8f61b926853b26fe1c1f233bc811d6b5a42b6
Mikhail Kobzarev <mikhail@kobzarev.com>
2023-03-18 19:58:25 +0300

    Добавляет класс с утилитами-хелперами

---
+++ src/Utils.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Вспомогательные утилиты.
+ *
+ * @package mytarget
+ */
+
+namespace VK\MyTracker;
+
+/**
+ * Класс Utils.
+ */
+class Utils {
+	/**
+	 * Get full plugin path.
+	 *
+	 * @return string
+	 */
+	public static function get_plugin_path(): string {
+		return VK_MYTRACKER_DIR_PATH;
+	}
+
+	/**
+	 * Get plugin basename.
+	 *
+	 * @return string
+	 */
+	public static function get_plugin_basename(): string {
+		return VK_MYTRACKER_DIR_BASENAME;
+	}
+
+	/**
+	 * Get plugin vesrion.
+	 *
+	 * @return string
+	 */
+	public static function get_plugin_version(): string {
+		return VK_MYTRACKER_VERSION;
+	}
+
+	/**
+	 * Get plugin file.
+	 *
+	 * @return string
+	 */
+	public static function get_plugin_file(): string {
+		return VK_MYTRACKER_FILE;
+	}
+
+	/**
+	 * Get plugin URL.
+	 *
+	 * @return string
+	 */
+	public static function get_plugin_url(): string {
+		return VK_MYTRACKER_DIR_URL;
+	}
+
+	/**
+	 * Get plugin asset URL.
+	 *
+	 * @param string $asset Asset path.
+	 *
+	 * @return string
+	 */
+	public static function get_plugin_asset_url( string $asset ): string {
+		return self::get_plugin_url() . 'assets/' . $asset;
+	}
+
+	/**
+	 * Get plugin slug.
+	 *
+	 * @return string
+	 */
+	public static function get_plugin_slug(): string {
+		return VK_MYTRACKER_SLUG;
+	}
+
+	/**
+	 * Get plugin prefix.
+	 *
+	 * @return string
+	 */
+	public static function get_plugin_prefix(): string {
+		return VK_MYTRACKER_PREFIX;
+	}
+
+	/**
+	 * Get plugin name.
+	 *
+	 * @return string
+	 */
+	public static function get_plugin_name(): string {
+		return VK_MYTRACKER_NAME;
+	}
+
+	/**
+	 * Get plugin DB version.
+	 *
+	 * @return string
+	 */
+	public static function get_db_version(): string {
+		return get_option( self::get_plugin_prefix() . '_version', '1.0.0' );
+	}
+
+	/**
+	 * Set plugin DB version.
+	 *
+	 * @param string $version Given version.
+	 * @return bool
+	 */
+	public static function set_db_version( string $version ): bool {
+		return update_option( self::get_plugin_prefix() . '_version', $version, false );
+	}
+
+	/**
+	 * Generate random key.
+	 *
+	 * @return string
+	 */
+	public static function generate_key(): string {
+		return str_replace( '-', '', wp_generate_uuid4() );
+	}
+
+	/**
+	 * Check if a string is JSON.
+	 *
+	 * @param mixed $string Input string.
+	 *
+	 * @return bool
+	 */
+	public static function is_json( $string ): bool {
+		return is_string( $string ) && is_array( json_decode( $string, true ) ) && ( json_last_error() === JSON_ERROR_NONE );
+	}
+}
