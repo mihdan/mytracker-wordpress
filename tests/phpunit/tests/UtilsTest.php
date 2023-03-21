753d6a0f3ea03881c949dfe5b3efc33130e41e84
Mikhail Kobzarev <mikhail@kobzarev.com>
2023-03-22 02:02:25 +0300

    Добавляет юнит-тесты для утилит

---
+++ tests/phpunit/tests/UtilsTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Class Test_Utils.
+ *
+ * @package mytracker
+ */
+
+namespace VK\MyTracker;
+
+use PHPUnit\Framework\TestCase;
+use tad\FunctionMocker\FunctionMocker;
+
+/**
+ * Класс для тестирования утилит.
+ */
+class UtilsTest extends TestCase {
+	/**
+	 * Test get_plugin_name().
+	 */
+	public function test_get_plugin_name() {
+		$plugin_name = 'MyTracker';
+		$constant    = FunctionMocker::replace( 'constant', $plugin_name );
+
+		self::assertSame( $plugin_name, Utils::get_plugin_name() );
+		$constant->wasCalledWithOnce( [ 'VK_MYTRACKER_NAME' ] );
+	}
+
+	/**
+	 * Test get_plugin_version().
+	 */
+	public function test_get_plugin_version() {
+		$version  = '1.0.0';
+		$constant = FunctionMocker::replace( 'constant', $version );
+
+		self::assertSame( $version, Utils::get_plugin_version() );
+		$constant->wasCalledWithOnce( [ 'VK_MYTRACKER_VERSION' ] );
+	}
+}
